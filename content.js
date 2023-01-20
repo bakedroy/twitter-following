c9e02e71bcba8a4ab74644807e3e92e8d8560d7d
bakedroy <bakedroy@tutanota.com>
2023-01-21 01:07:15 +0900

    Update content script using promise

--- content.js
+++ content.js
@@ -1,54 +1,29 @@
-function sleep(waitSec, callbackFunc) {
-	var spanedSec = 0;
-
-	var waitFunc = function() {
-		spanedSec++;
-		if (spanedSec >= waitSec) {
-			if (callbackFunc) callbackFunc();
-			return;
-		}
-		clearTimeout(id);
-		id = setTimeout(waitFunc, 1000);
-	};
-
-	var id = setTimeout(waitFunc, 1000);
+function waitForElement(querySelector, timeout){
+	return new Promise((resolve, reject)=>{
+		var timer = false;
+		if (document.querySelectorAll(querySelector).length) return resolve();
+		const observer = new MutationObserver(()=>{
+			if (document.querySelectorAll(querySelector).length){
+				observer.disconnect();
+				if (timer !== false) clearTimeout(timer);
+				return resolve();
+			}
+		});
+		observer.observe(document.body, {
+			childList: true,
+			subtree: true
+		});
+		if (timeout) timer = setTimeout(()=>{
+			observer.disconnect();
+			reject();
+		}, timeout);
+	});
 }
 
-sleep(1, function() {
-	var a = document.querySelectorAll('a[href="/home"]');
-	if (a.length > 0) {
-		for (var i = 0, l = a.length; i < l; i++) {
-			// i = 2: For you
-			// i = 3: Following
-			if (i == 3) { a
-				a[i].click();
-				//console.log(a[i]);
-			}
-		}
-	} else {
-		console.log('<a href="/home"> not found');
-	}
+console.log("loaded");
+
+waitForElement('[href="/home"][role="tab"][aria-selected="false"]', 1000).then(()=> {
+	var a = document.querySelectorAll('[href="/home"][role="tab"][aria-selected="false"]');
+	console.log(a.length);
+	a[0].click();
 });
-
-
-//// XXX: only applicable to one element
-//if (r.length == 1) {
-//  r[0].onload = function() {
-//    onLoaded(r[0]);
-//  }
-//}
-//
-//// Trigerred if the element(r) is loaded.
-//// Do something at here.
-//function onLoaded(r) {
-//  var f = r.contentDocument.getElementsByTagName('form');
-//  const names = ['aite', 'dmy04', 'dmy05']; // change me what u want
-//
-//  for (var i = 0, l = f.length; i < l; i++) {
-//    for (var j = 0, jl = names.length; j < jl; j++) {
-//      if (f[i].name == names[j]) {
-//        f[i].rel = "opener"; // inherit properties to children
-//      }
-//    }
-//  }
-//}
